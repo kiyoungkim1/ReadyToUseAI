09e4f86feb3131bee4331c1f9eca0d3f236facbd
Kiyoung Kim <kky416@gmail.com>
2021-03-13 01:10:37 +0900

    add find_simila_sentence in tfidf.py

--- src/nlp/tfidf.py
+++ src/nlp/tfidf.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
+import numpy as np
 
 def apply(dataset_path='dataset.txt', save_path='tfidf.xlsx'):
     # read dataset
@@ -22,9 +23,38 @@ def apply(dataset_path='dataset.txt', save_path='tfidf.xlsx'):
     total = df.sum().to_frame(name='total').T
     df = pd.concat([total, df])
 
-
     df.sort_values(by=['total'], ascending=False, axis=1)
-    df.to_excel(save_path)
-    print('Result is saved at {}'.format(save_path))
+    
+    if save_path:
+        df.to_excel(save_path)
+        print('Result is saved at {}'.format(save_path))
+
+    return df, vect
+
+
+def cosine_similarity(a, b):
+    sim =  np.dot(a, b) / (np.linalg.norm(a) * (np.linalg.norm(b)))
+    return sim[0]
+
+def find_similar_sentence(text, dataset_path='dataset.txt', save_path='tfidf_similarity.xlsx'):
+    df, vect = apply(dataset_path='dataset.txt', save_path=None)
+    
+    # get cosine similarity
+    text_tokenized = vect.transform([text]).toarray()
+    df['similarity'] = df.index.map(lambda x: cosine_similarity(text_tokenized, df.loc[x, :].values))
+    
+    # similarity column as first column
+    col = list(df.columns)
+    value = col.pop()
+    col.insert(0, value)
+
+    df = df[col]
+
+    # sort by similarity
+    df.sort_values(by=['similarity'], ascending=False, axis=0, inplace=True)
+
+    if save_path:
+        df.to_excel(save_path)
+        print('Result is saved at {}'.format(save_path))
 
     return df
